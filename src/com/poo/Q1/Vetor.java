e0498737119f7a21d3efb6d29735eb3e25d4d317
Matheus Cavalcante <jmatheusoc269@gmail.com>
2022-10-07 15:08:29 -0300

    Questão 1 de revisão
    resolvendo questões para ajudar no desenvolvimento da matéria de estrutura

---
+++ src/com/poo/Q1/Vetor.java
@@ -0,0 +1,14 @@
+package com.poo.Q1;
+
+public class Vetor {
+    //int[] vetorDeInteiros;
+
+//    contando de modo interativo
+    public int contarDadosVetor(int[] vetorr) {
+        int tamanho = 0;
+        for (int i = 0; i < vetorr.length; i++) {
+            tamanho++;
+        }
+        return tamanho;
+    }
+}
---
+++ src/com/poo/Q1/main.java
@@ -0,0 +1,13 @@
+package com.poo.Q1;
+
+public class main {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor();
+
+
+        int[] numerosAleatorios = {10, 60, 50};
+
+        System.out.println(vetor.contarDadosVetor(numerosAleatorios));
+        ;
+    }
+}
