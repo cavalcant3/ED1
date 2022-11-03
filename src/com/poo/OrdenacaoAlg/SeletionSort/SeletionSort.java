75371cef6c6dde70ed81cdc989723a6630397c94
Matheus Cavalcante <jmatheusoc269@gmail.com>
2022-11-03 10:35:02 -0300

    SeletionSort - Complexidade

--- src/com/poo/OrdenacaoAlg/SeletionSort/SeletionSort.java
+++ src/com/poo/OrdenacaoAlg/SeletionSort/SeletionSort.java
@@ -7,29 +7,29 @@ public class SeletionSort {
 
         // SeletionSort seletionSort = new SeletionSort();
 
-        int vetor[] = new int[10];
+        int vetor[] = new int[3];
 
 
-        Scanner ler = new Scanner(System.in);
 
 //        preenchendo dados do meu array
         for (int i = 0; i < vetor.length; i++) {
-            vetor[i] = 1 + (int) (Math.random() * 100);
+            vetor[i] = 1 + (int) (Math.random() *100);
         }
         System.out.println("Vetor desordenado: ");
-        for (int i = 0; i < vetor.length; i++) {
+        for (int i = 0; i < vetor.length; i++) { //o(n)
             System.out.println(vetor[i]);
         }
+
 
 //        seletion sort
 
 
         int posicaoMenor, aux;
 //        percorre o vetor desordenado
-        for (int i = 0; i < vetor.length; i++) {
+        for (int i = 0; i < vetor.length; i++) { //o(n)
             posicaoMenor = i;
 //            encontra a menor posição
-            for (int j = i + 1; j < vetor.length; j++) {
+            for (int j = i + 1; j < vetor.length; j++) { //o(n)
                 if (vetor[j] < vetor[posicaoMenor]) {
                     posicaoMenor = j;
                 }
@@ -39,8 +39,11 @@ public class SeletionSort {
             vetor[posicaoMenor] = vetor[i];
             vetor[i] = aux;
         }
+//        Comlpexidade:
+        //O(N) * O(N)
+        //O(N²)
 
-
+//      imprimindo o array
         System.out.println("Vetor ordenado: ");
         for (int i = 0; i < vetor.length; i++) {
             System.out.println(vetor[i]);
